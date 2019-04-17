74e420b1f39e96dd915204f8a4eb140645511775
andrydiurs <a.durso@studenti.unibs.it>
2019-04-17 19:01:38 +0200

    test per controllore

--- src/unibs/fp/it/codicefiscale/CodiciFiscaliMain.java
+++ src/unibs/fp/it/codicefiscale/CodiciFiscaliMain.java
@@ -1,6 +1,7 @@
 package unibs.fp.it.codicefiscale;
 
 import java.io.FileInputStream;
+import java.util.*;
 import java.util.ArrayList;
 
 import javax.xml.stream.XMLInputFactory;
@@ -8,11 +9,13 @@ import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamReader;
 
 public class CodiciFiscaliMain {
+	private static final String SCHEMA_CODICE_FISCALE = "[a-zA-Z]{6}\\d\\d[a-zA-Z]\\d\\d[a-zA-Z]\\d\\d\\d[a-zA-Z]";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 		Dati d = new Dati();
-		d.leggiInputPersone(p);
+		//d.leggiInputPersone(p);
 	    Persona p= new Persona();
 		//System.out.println("messaggio" + d.leggiInputPersone() );
 		//Persona p = new Persona();
@@ -21,7 +24,19 @@ public class CodiciFiscaliMain {
 		persone.add(p);
 		p.setCognome("ponzin");
 		persone.add(p);*/
-		p.stampa(d.);
+		//p.stampa(d.);
+		
+	    Scanner scanner = new Scanner (System.in);
+	    Controllore controllore = new Controllore();
+	    String provaCodice = scanner.next();
+	    
+	    if (controllore.controllaFormato(SCHEMA_CODICE_FISCALE, provaCodice)) {
+	      System.out.printf("Formato del codice fiscale VALIDO");
+	    }else {
+	    	System.out.printf("Formato del codice fiscale NON VALIDO");
+	    }
+		
+		
+		
 		}
-
 }
