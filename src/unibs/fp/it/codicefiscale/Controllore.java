ee8e7468a1cacaab3060f9c7dfb2a0b07bb67dc4
andrydiurs <a.durso@studenti.unibs.it>
2019-04-17 18:26:19 +0200

    controllo validità codice F.

--- src/unibs/fp/it/codicefiscale/Controllore.java
+++ src/unibs/fp/it/codicefiscale/Controllore.java
@@ -1,5 +1,34 @@
 package unibs.fp.it.codicefiscale;
 
-public class Controllore {
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
+
+public class Controllore {
+	private static final String SCHEMA_CODICE_FISCALE = "/^(?:(?:[B-DF-HJ-NP-TV-Z]|[AEIOU])[AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}[\\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[1256LMRS][\\dLMNP-V])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM])(?:[A-MZ][1-9MNP-V][\\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i";
+	Dati codiceFiscale = new Dati ();
+	
+	
+	/**
+	 * 
+	 * @param SCHEMA_CODICE_FISCALE: schema con tutte le caratteristiche che deve avere un codice fiscale
+	 * @param codiceDaControllare: codice fiscale da passere al metodo e che deve essere controllato
+	 * @return ritorna true se il codice fiscale è valido, false altrimenti
+	 */
+	public boolean controllaFormato (String SCHEMA_CODICE_FISCALE, String codiceDaControllare) {    //VEDETE COME PASSARE IL CODICE FISCALE AL METODO CHE IO NON LO SO
+		if(Pattern.matches(SCHEMA_CODICE_FISCALE, codiceDaControllare)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
