b0180f8938f07bb10561fa7b2a29e86bb0429474
Diego <d.rossi025@studenti.unibs.it>
2019-04-19 17:55:00 +0200

    crea cognome, quasi il nome, anno

--- src/unibs/fp/it/codicefiscale/Creazione.java
+++ src/unibs/fp/it/codicefiscale/Creazione.java
@@ -18,17 +18,26 @@ public class Creazione {
 	}
 	
 	
+//////////////////////////////////////////////////////////////////////////////
+	//crea il codice iniziale di 3 caratteri dal cognome
 	private String creaCodiceCognome(String cognomePersona) {
 		int i = 0;
 		char[] codiceCognomePersona = new char[3];
 	
-		codiceCognomePersona = estraiPrimeTreConsonanti(cognomePersona, i);
+		codiceCognomePersona = estraiPrimeTreConsonanti(cognomePersona);
 		
 		//se vengono trovate 3 consonanti viene ritornato il codice composto da 3 consonanti
-		if (i == 3) return codiceCognomePersona.toString();
+		if (codiceCognomePersona[2] != 0) return codiceCognomePersona.toString();
+			else {
+				if (codiceCognomePersona[0] == 0) i = 0;
+				else
+					if (codiceCognomePersona[1] == 0) i = 1;
+					else if (codiceCognomePersona[2] == 0) i = 2;
+				}
 		
 		//se non vengono trovate 3 consonanti si passa a questo for, che cerca le vocali e riempie gli eventuali spazi svuoti
 		for (int j = 0; j < cognomePersona.length(); j++) {
+					
 			if (isConsonant(cognomePersona.charAt(j)) == false) {
 				codiceCognomePersona[i] = cognomePersona.charAt(j);
 				i++;
@@ -40,7 +49,7 @@ public class Creazione {
 		return riempiSpaziX(i, codiceCognomePersona).toString();
 	}
 	
-	
+////////////////////////////////////////////////////////////////////////////
 	private String creaCodiceNome(String nomePersona) {
 		int i = 0;
 		char[] codiceNomePersona = new char[3];
@@ -48,7 +57,7 @@ public class Creazione {
 			
 			
 		} else {
-			codiceNomePersona = estraiPrimeTreConsonanti(nomePersona, i);
+			codiceNomePersona = estraiPrimeTreConsonanti(nomePersona);
 		}
 		
 		
@@ -56,6 +65,13 @@ public class Creazione {
 	}
 
 
+/////////////////////////////////////////////////////////////////////////////
+//crea una sottostringa di due numeri da un int preso in ingresso e la ritorna
+	public static String creaCodiceAnno(int annoPersona) {	
+	return String.valueOf(annoPersona).substring(2);
+	}
+	
+	
 	private char[] riempiSpaziX(int k, char[] codiceConSpaziVuoti) {
 		char[] codiceDaRiempire = codiceConSpaziVuoti;
 		//se anche le vocali sono insufficienti si riempiono gli spazi rimasti con delle 'X'
@@ -66,11 +82,10 @@ public class Creazione {
 		return codiceDaRiempire;
 	}
 	
-
 	
-	private char[] estraiPrimeTreConsonanti(String datoPersona, int k) {
+	public static char[] estraiPrimeTreConsonanti(String datoPersona) {
 		char[] codicePrimeTreConsonanti = new char[3];
-		
+		int k = 0;
 		//scorre il cognome (o nome) della persona preso in ingresso
 		for (int j = 0; j < datoPersona.length() ; j++) {
 			if (isConsonant(datoPersona.charAt(j))) {
@@ -78,30 +93,34 @@ public class Creazione {
 				k++;
 			}
 			
-			//se vengono trovate 3 consonanti viene restituito l'array dei 3 caratteri
+			//se si arriva già a 3 caratteri estratti non serve controllare anche le altre lettere
 			if (k == 3) return codicePrimeTreConsonanti;
+			
 		}
 		return codicePrimeTreConsonanti;
 	}
 	
 	//conta le consonanti contenute in una parola (usato per il metodo creaCodiceNome)
-	private int contaConsonanti(String parola) {
-		int numConsonanti = 0;
-		//si scorre la parola per controllare ogni lettera
-		for (int i = 0; i < parola.length(); i++) {
-			if (isConsonant(parola.charAt(i))) numConsonanti++;		
+		public static int contaConsonanti(String parola) {
+			int numConsonanti = 0;
+			//si scorre la parola per controllare ogni lettera
+			for (int i = 0; i < parola.length(); i++) {
+				if (isConsonant(parola.charAt(i))) numConsonanti++;		
+			}
+			return numConsonanti;
 		}
-		return numConsonanti;
-	}
-	
-	
-	
-	
-	//controlla che un carattere dato in ingresso sia una consonante
-	private boolean isConsonant(char carattere) {
-		if (carattere == 'a' || carattere == 'e' || carattere == 'i' || carattere == 'o' || carattere == 'u' || carattere == 'A' || carattere == 'E' || carattere == 'I' || carattere == 'O' || carattere == 'U') return false;
-			else return true;
-	}
-	
-
+		
+		
+		
+		
+		//controlla che un carattere dato in ingresso sia una consonante
+		public static boolean isConsonant(char carattere) {
+			if (carattere != 'a' || carattere == 'e' || carattere == 'i' || carattere == 'o' || carattere == 'u' || carattere == 'A' || carattere == 'E' || carattere == 'I' || carattere == 'O' || carattere == 'U') return false;
+				else return true;
+		}
+		
+		
+		public void stampaCodice(char[] codiceDaStampare) {
+			System.out.println(codiceDaStampare.toString());
+		}
 }
