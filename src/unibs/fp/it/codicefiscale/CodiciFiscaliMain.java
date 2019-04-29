7991f66c6971ce7d222aaabed8cc2665ee9d0129
andrydiurs <a.durso@studenti.unibs.it>
2019-04-29 11:57:55 +0200

    quasi fatto

--- src/unibs/fp/it/codicefiscale/CodiciFiscaliMain.java
+++ src/unibs/fp/it/codicefiscale/CodiciFiscaliMain.java
@@ -14,16 +14,16 @@ public class CodiciFiscaliMain {
 
 	public static void main(String[] args) {
 		ArrayList <Persona> listaDiPersone;
-		ArrayList<String> codiciPersone = new ArrayList<String>();
+		ArrayList <String> codiciPersone = new ArrayList<String>();
 		
 		Dati d = new Dati();
-		/*
+		
 		listaDiPersone = d.leggiInputPersone(); 
 		for (int k = 0; k < listaDiPersone.size(); k++) {
 			codiciPersone.add(Creazione.creaCodice(listaDiPersone.get(k)));
 			//System.out.println(Creazione.creaCodice(listaDiPersone.get(k)));
 		}
-		*/
+		
 		//System.out.println(listaDiPersone);
 		ArrayList<CodiceFiscale> codiciLetti = new ArrayList<CodiceFiscale>();
 		codiciLetti = d.leggiCodiciFiscali();
@@ -40,16 +40,42 @@ public class CodiciFiscaliMain {
 			
 		}
 		
-		
-		
+		int contatorePresente = 0;
+		int contatoreAssente = 0;
 		/*
-		for (int k = 0; k < codiciLetti.size(); k++) {
-			
-			System.out.println(d.leggiCodiciFiscali());
-		}
+			for (int i = 0; i < listaDiPersone.size(); i++) {
+				for (int k=0; k < codiciLetti.size(); k++) {
+				if (codiciLetti.get(k).toString().equals(codiciPersone.get(i))) {
+					contatorePresente++;
+				}
+				
+				
+			    }
+			}
 		*/
+		
+		
+		ArrayList <String> codiciLettiString = new ArrayList<String>();
+		for (int i = 0; i < codiciLetti.size(); i++) {
+			codiciLettiString.add(codiciLetti.get(i).toString());
+		}
+		
+		
+		for (int i = 0; i < codiciPersone.size(); i++) {
+			if (codiciLettiString.contains(codiciPersone.get(i))) {
+				System.out.println(codiciPersone.get(i)+" CONTENUTO");
+				contatorePresente++;
+			}else {
+				System.out.println(codiciPersone.get(i)+" NON C'é");
+				contatoreAssente++;
+			}
+		}
+		
+		
+		
 		System.out.println("I codici giusti sono: " +contatoreGiusti);
 		System.out.println("I codici sbagliati sono: " +contatoreSbagliati);
+		System.out.println("I codici presenti sono "+ contatorePresente+" I codici non presenti sono :"+contatoreAssente);
 		
 		}
 }
